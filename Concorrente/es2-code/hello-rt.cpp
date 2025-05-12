0b36de3984e6c43b798846bcf4553e45e6c2f9e3
avvo21 <avvo21@gmail.com>
2025-05-12 18:59:30 +0200

    Aggiunta della gestione dell'affinità del thread per migliorare le prestazioni; impostato l'affinità per il processore 0 e stampato l'affinità dei thread.

--- Concorrente/es2-code/hello-rt.cpp
+++ Concorrente/es2-code/hello-rt.cpp
@@ -31,6 +31,14 @@ int main()
 	
 	std::list<std::thread> threads;
 
+	rt::affinity single_cpu;
+    single_cpu.reset();       // pulisce tutti i bit
+    single_cpu.set(0, true);  // abilita solo il processore 0
+	
+	rt::this_thread::set_affinity(single_cpu);
+    auto aff = rt::this_thread::get_affinity();
+    std::cout << aff << std::endl;
+
 	for(size_t id = 0; id < hello.size(); ++id)
 	{
 		std::thread th(print_char, id, std::ref(b));
@@ -38,6 +46,8 @@ int main()
 		try{
 			rt::priority p(rt::priority::rt_max - id);
 			rt::set_priority(th, p);
+			rt::set_affinity(th, single_cpu);
+			std::cout << id << ": \t" << rt::get_affinity(th) << std::endl;
 		}
 		catch(rt::permission_error &e){
 			std::cout << "Failed to set priority: " << e.what() << std::endl;
